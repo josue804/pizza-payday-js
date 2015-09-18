bac9b34f84cb69cce3a50b6fc10bf8a52bc678b5
Josue Valverde <student@epicodus.com>
2015-09-18 10:59:01 -0700

    setup pizza html page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function Pizza(size, toppings) {
-  this.pizzaSize = size;
+function Pizza(toppings, size) {
   this.toppings = toppings;
+  this.pizzaSize = size;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,7 @@
 describe("Pizza", function() {
   it("Creates a pizza of the appropriate size and with the appropriate toppings", function() {
-    var testPizza = new Pizza("XXL", ["green peppers", "feta cheese", "mushrooms"]);
-    expect(testPizza.pizzaSize).to.equal("XXL");
+    var testPizza = new Pizza(["green peppers", "feta cheese", "mushrooms"], "Extra Large");
     expect(testPizza.toppings).to.eql(["green peppers", "feta cheese", "mushrooms"]);
+    expect(testPizza.pizzaSize).to.equal("Extra Large");
   });
 });
