494f81fa413a47debf02c9a0122c4f4fe0618240
Josue Valverde <student@epicodus.com>
2015-09-18 11:12:53 -0700

    add price calculator function to pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@ function Pizza(toppings, size) {
   this.toppings = toppings;
   this.pizzaSize = size;
 }
+
+Pizza.prototype.price = function() {
+  return ((this.toppings.length - 1) * 2.5) + this.pizzaSize * 3.5;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,12 @@
 describe("Pizza", function() {
   it("Creates a pizza of the appropriate size and with the appropriate toppings", function() {
-    var testPizza = new Pizza(["green peppers", "feta cheese", "mushrooms"], "Extra Large");
+    var testPizza = new Pizza(["green peppers", "feta cheese", "mushrooms"], 4);
     expect(testPizza.toppings).to.eql(["green peppers", "feta cheese", "mushrooms"]);
-    expect(testPizza.pizzaSize).to.equal("Extra Large");
+    expect(testPizza.pizzaSize).to.equal(4);
+  });
+
+  it("calculates the cost of a pizza based off toppings and size", function() {
+    var testPizza = new Pizza(["pepperoni", "italian sausage", "bacon", "mozzarella"], 4);
+    expect(testPizza.price()).to.equal(21.50);
   });
 });
